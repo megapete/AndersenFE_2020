ef0b5ed0432d9b925aa0f9143fefdd00e06bda6a
Peter C Huber <peter@hubersitech.com>
2020-07-17 14:08:33 -0400

    Initial Commit

---
+++ AndersenFE_2020/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  AndersenFE_2020
+//
+//  Created by Peter Huber on 2020-07-17.
+//  Copyright © 2020 Peter Huber. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+

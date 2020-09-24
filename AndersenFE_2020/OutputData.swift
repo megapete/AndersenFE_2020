55733d2699c1b8598541c5f31e7582a338266f7e
Peter C Huber <peter@hubersitech.com>
2020-09-24 14:02:28 -0400

    Working on OuputData

--- AndersenFE_2020/OutputData.swift
+++ AndersenFE_2020/OutputData.swift
@@ -14,6 +14,8 @@ struct OutputData {
     
     let MVA:Double
     
+    let impedance:Double
+    
     struct TermData
     {
         let termNum:Int
@@ -25,7 +27,7 @@ struct OutputData {
     
     struct LayerData
     {
-        let windingID:String
+        let windingDesc:String
         
         let parentTerminal:Int
         
@@ -42,10 +44,42 @@ struct OutputData {
         
         let maxCombinedForce:Double
         
-        let dcLoss:Double
-        let averageEddyLoss:Double
-        let maximumEddyLoss:Double
+        private let dcLoss75:Double
+        private let averageEddyPU75:Double
+        private let maximumEddyPU75:Double
         let maxEddyLossRect:NSRect
+        
+        // Andersen calculates losses at 75C
+        static let refTemp = 75.0
+        
+        func dcLoss(temp:Double) -> Double
+        {
+            return self.dcLoss75 * (234.5 + temp) / (234.5 + LayerData.refTemp)
+        }
+        
+        func aveEddyLoss(temp:Double) -> Double
+        {
+            let refEddyLoss = self.dcLoss75 * self.averageEddyPU75
+            
+            return refEddyLoss * (234.5 + LayerData.refTemp) / (234.5 + temp)
+        }
+        
+        func maxEddyLoss(temp:Double) -> Double
+        {
+            let refEddyLoss = self.dcLoss75 * self.maximumEddyPU75
+            
+            return refEddyLoss * (234.5 + LayerData.refTemp) / (234.5 + temp)
+        }
+        
+        func aveEddyPU(temp:Double) -> Double
+        {
+            return self.aveEddyLoss(temp: temp) / self.dcLoss(temp: temp)
+        }
+        
+        func maxEddyPU(temp:Double) -> Double
+        {
+            return self.maxEddyLoss(temp: temp) / self.dcLoss(temp: temp)
+        }
         
     }
     
@@ -60,8 +94,27 @@ struct OutputData {
         
         self.description = outputDesc
         self.MVA = results.baseMVA
+        self.impedance = results.puImpedance
         
-        self.terminals = []
+        var termData:[TermData] = []
+        let availableTermNums = txfo.AvailableTerminals()
+        for nextTermNum in availableTermNums
+        {
+            let tryMVA = try? txfo.TotalVA(terminal: nextTermNum)
+            let tryV = try? txfo.TerminalLineVoltage(terminal: nextTermNum)
+            if tryMVA == nil || tryV == nil
+            {
+                return nil
+            }
+            
+            let nextTermData = TermData(termNum: nextTermNum, termMVA: tryMVA!, termKV: tryV!)
+            termData.append(nextTermData)
+        }
+        self.terminals = termData.sorted(by: {$0.termNum < $1.termNum})
+        
+        var layerData:[LayerData] = []
+        
+        
         self.layers = []
     }
 }

ff7a401853145cceebc39b33cae795d5bdfab867
Peter C Huber <peter@hubersitech.com>
2020-10-01 19:53:20 -0400

    Gaaaah! NSTableView! Gaaaah!

--- AndersenFE_2020/EditOutputDataDialog.swift
+++ AndersenFE_2020/EditOutputDataDialog.swift
@@ -8,9 +8,30 @@
 
 import Cocoa
 
-class EditOutputDataDialog: PCH_DialogBox {
+class EditOutputDataDialog: PCH_DialogBox, NSTableViewDataSource, NSTableViewDelegate {
+        
+    @IBOutlet weak var outDataTableView: NSTableView!
     
-    @IBOutlet weak var tableView: NSTableView!
+    var outputDataStrings:[String] = []
     
-
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        
+        return self.outputDataStrings.count
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        
+        var result:NSTextField? = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "OutputData"), owner: self) as? NSTextField
+        
+        if result == nil
+        {
+            result = NSTextField(frame: NSRect(x: 0.0, y: 0.0, width: tableView.frame.width, height: 10.0))
+            
+            result!.identifier = NSUserInterfaceItemIdentifier(rawValue: "OutputData")
+        }
+        
+        result!.stringValue = self.outputDataStrings[row]
+        
+        return result
+    }
 }
